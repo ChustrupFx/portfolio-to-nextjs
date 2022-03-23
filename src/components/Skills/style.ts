789cc82783211c9e9d414ea9519759178f823fe3
Victor Inácio <chustrupgamer@gmail.com>
2022-03-23 16:20:09 -0300

    Skills sections is now responsive

--- src/components/Skills/style.ts
+++ src/components/Skills/style.ts
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import mediaQueries from "../../styles/mediaQueries";
 
 export const SkillsContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 3rem;
   grid-column-gap: 5rem;
   width: 100%;
+
+  @media (max-width: ${mediaQueries.large}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${mediaQueries.medium}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 `;
 
 export const SkillItem = styled.div`
