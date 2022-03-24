3cf10369711667d673a750275a3efa8ad40676b5
Victor Inácio <chustrupgamer@gmail.com>
2022-03-24 15:47:28 -0300

    About me section animations finished

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,14 +3,16 @@ import { Paragraph } from "./style";
 const AboutMe: React.FC = () => {
   return (
     <>
-      <Paragraph>Meu nome é Victor, tenho 18 anos. </Paragraph>
-      <Paragraph>
+      <Paragraph data-aos="fade-up">
+        Meu nome é Victor, tenho 18 anos.{" "}
+      </Paragraph>
+      <Paragraph data-aos="fade-up">
         Comecei a estudar programação em 2018 e foi aí que descobri a minha
         paixão. Desde então venho estudando as tecnologias mais atuais do
         mercado, tentando sempre me atualizar e me adaptar as novidades do mundo
         da programação.
       </Paragraph>
-      <Paragraph>
+      <Paragraph data-aos="fade-up">
         Hoje em dia me aventuro sendo freelancer, sempre aceitando novos
         desafios e estando a total disposição dos meus clientes.
       </Paragraph>
