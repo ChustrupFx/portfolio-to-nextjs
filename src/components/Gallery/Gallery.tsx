26624ca5aac5df8ebb238864bd93f6fae5a49d9b
Victor Inácio <chustrupgamer@gmail.com>
2022-03-28 13:10:20 -0300

    Images compression

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -6,36 +6,36 @@ const Gallery: React.FC = () => {
   return (
     <GalleryContent>
       <GalleryItem
-        coverImage="/images/franquiamaisagil.jpg"
+        coverImage="/images/franquiamaisagil.webp"
         alt="Franquia Mais Agil"
         description="Website Franquia Mais Ágil"
         visitLink="https://franquiamaisagil.com"
       />
       <GalleryItem
-        coverImage="/images/seg-pet.png"
+        coverImage="/images/seg-pet.webp"
         alt="Seg Pet"
         description="Website Seg Pet"
         visitLink="https://seg.pet"
       />
       <GalleryItem
-        coverImage="/images/espiritismoeluz.png"
+        coverImage="/images/espiritismoeluz.webp"
         alt="Espiritismo e Luz"
         description="Website Espiritismo é Luz"
         visitLink="https://espiritismoeluz.com.br/"
       />
       <GalleryItem
-        coverImage="/images/dominandooexcel.png"
+        coverImage="/images/dominandooexcel.webp"
         alt="Dominando o Excel"
         description="Website Dominando Excel"
         visitLink="https://dominandoexcel.com.br/"
       />
       <GalleryItem
-        coverImage="/images/interpretacao-de-texto-landing-page.png"
+        coverImage="/images/interpretacao-de-texto-landing-page.webp"
         alt="Landing Page Interpretacao de Texto"
         description="Landing Page Curso de Interpretação de Texto"
       />
       <GalleryItem
-        coverImage="/images/segpet-form.png"
+        coverImage="/images/segpet-form.webp"
         alt="Seg Pet Form"
         description="Formulário Seg Pet"
         visitLink="https://pet.esegurotech.com.br/enroll/create?plan=93"
